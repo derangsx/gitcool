38a273683ce651f59999c86f2fc25b1ed51d1b6f
testuser1 <testuser1@gmail.com>
2020-04-15 21:01:33 -0400

    Added sudo code and list of users in system

--- details.sh
+++ details.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 echo "This will provide information about the local system"
+echo
+#Gives HOSTNAME
 echo $(hostname)
+echo
+#GIVES USER EXECUTED THIS SCRIPT
 echo $(whoami)
-echo $(id -un)
+echo
+#GIVES USERS IN SYSTEM
+USERS=$(cat /etc/passwd | cut -d ":" -f 1)
+echo $USERS
